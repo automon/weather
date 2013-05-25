4c0ca9acca194278805d1c0616b1f23c0690c4c1
Matt Mueller <mattmuelle@gmail.com>
2013-05-25 02:47:22 -0700

    wrapped

--- index.js
+++ index.js
@@ -8,14 +8,15 @@ var request = require('superagent');
  * Make the request
  */
 
-request
-  .get('http://api.openweathermap.org/data/2.5/weather?q=San+Francisco')
-  .end(function(err, res) {
-    if(err) throw err;
-    var body = JSON.parse(res.text);
-    console.log(toF(body.main.temp) + '˚');
-  });
-
+module.exports = function(json, done) {
+  request
+    .get('http://api.openweathermap.org/data/2.5/weather?q=San+Francisco')
+    .end(function(err, res) {
+      if(err) throw err;
+      var body = JSON.parse(res.text);
+      console.log(toF(body.main.temp) + '˚');
+    });
+}
 
 function toF(k) {
   return (k - 273.15) * 1.8 + 32 | 0;
