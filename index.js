84c2d725752547b67edb74a9b74db514b53da195
Matt Mueller <mattmuelle@gmail.com>
2013-06-01 10:13:45 -0700

    added debug

--- index.js
+++ index.js
@@ -3,18 +3,22 @@
  */
 
 var request = require('superagent');
+var debug = require('automon:weather');
 
 /**
  * Make the request
  */
 
 module.exports = function(city, done) {
+  debug('GET %s', 'http://api.openweathermap.org/data/2.5/weather?q=' + city)
   request
     .get('http://api.openweathermap.org/data/2.5/weather?q=' + city)
     .end(function(err, res) {
       if(err) return done(err);
       var body = JSON.parse(res.text);
-      done(null, toF(body.main.temp));
+      var f = toF(body.main.temp);
+      debug('%s˚', f);
+      done(null, f);
     });
 }
 
